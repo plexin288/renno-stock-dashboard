2b6e160f502681c8d3b31b3877ce6b41907e0717
plexin288 <rennoglory@gmail.com>
2026-05-21 16:14:54 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -42,20 +42,8 @@ header {
 }
 
 [data-testid="collapsedControl"] {
-    display: flex !important;
-    visibility: visible !important;
-    opacity: 1 !important;
-    z-index: 999999 !important;
-
-    position: fixed !important;
-    top: 20px !important;
-    left: 20px !important;
-
-    background: white !important;
-    border-radius: 12px !important;
-    padding: 8px !important;
-
-    box-shadow: 0 4px 20px rgba(0,0,0,0.1);
+    display: block !important;
+    z-index: 99999 !important;
 }
 
 section[data-testid="stSidebar"] {
